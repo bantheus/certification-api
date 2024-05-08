d3d959b3504b0cc52e02b0b0dc70c91083eee8ef
bantheus <matheuschmidt.b@gmail.com>
2024-05-08 15:56:22 -0300

    ✨ feat: add getters, setters and constructors

--- src/main/java/com/bantheus/certification/modules/students/entities/AnswersCertificationEntity.java
+++ src/main/java/com/bantheus/certification/modules/students/entities/AnswersCertificationEntity.java
@@ -2,6 +2,13 @@ package com.bantheus.certification.modules.students.entities;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnswersCertificationEntity {
   private UUID id;
   private UUID certificationId;
--- src/main/java/com/bantheus/certification/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/bantheus/certification/modules/students/entities/CertificationStudentEntity.java
@@ -3,6 +3,13 @@ package com.bantheus.certification.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CertificationStudentEntity {
   private UUID id;
   private UUID studentId;
